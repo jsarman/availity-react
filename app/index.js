6e4af902a20220fd548b8ab6d56eff694265fef7
John Sarman <John.Sarman@availity.com>
2015-12-11 10:50:48 -0500

    Removed react-router from demo

--- app/index.js
+++ app/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import { Router, Route, Link, Redirect } from 'react-router'
-import { createHistory, useBasename } from 'history'
 import rootReducer from './reducers'
 
 import Page1 from './components/page1'
@@ -12,23 +10,20 @@ import Page2 from './components/page2'
 import uikit from 'availity-uikit'
 
 require('jquery');
-const history = useBasename(createHistory)({
-    basename: '/'
-})
 
 class App extends React.Component {
     render() {
         return (
-            <div className="container-sm">
-        <h1 className="page-title">Welcome</h1>
-                <div role="tabpanel">
-            <ul className="nav nav-tabs" role="tablist">
-                <li><Link to="/page1" activeClassName="active">Page 1</Link></li>
-               <li><Link to="/page2" activeClassName="active">Page 2</Link></li>
-            </ul>
-            </div>
-            {this.props.children}
-          </div>
+            <Provider store={store}>
+                <div className="container-xs">
+                    <div className="row">
+                        <div className="col-xs-6 col-xs-offset-3">
+                            <h1 className="page-title">Welcome</h1>
+                            <Page1 />
+                        </div>
+                    </div>
+                </div>
+            </Provider>
             )
     }
 }
@@ -38,12 +33,5 @@ let store = createStore(rootReducer);
 
 
 render((
-    <Provider store={store}>
-    <Router history={history}>
-    <Route path="/" component={App}>
-      <Route path="page1" component={Page1} />
-      <Route path="page2" component={Page2} />
-    </Route>
-  </Router>
-  </Provider>
+    <App />
     ), document.getElementById('main'))
